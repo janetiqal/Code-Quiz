b4dd972163e1198a4fa7c80d1597d01f5d891e6b
Janet Iqal <j.iqal35@gmail.com>
2021-06-25 23:01:34 -0500

    updated readme, css

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Homework-04
+# Name of Assignment 
 Code Quiz 
+# Purpose
+The purpose of this assignment is to build a coding quiz with questions with multiple answer options for users to test their coding knowledge. This assignment requires knowledge of HTML, CSS, Javascript and Web-API's to build a functional, responsive quiz. 
+# My Experience
+The first thing I did was structure my html to create the layout of the code quiz. I created a header, tile, and mutlpiple div tags to place the question and the questions answers. I then created constant variables for them for me to refrence in my javascript file. I built two variables as arrays. Variable for questions, and variable for answers. 
+# Link to App
+# Screen Shots 
+# Created By
+Janet Iqal
--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,42 @@
-const question= document.getElementById("question");
-const optionA= document.getElementById("optionA");
-const optionB= document.getElementById("optionB");
-const optionC= document.getElementById("optionC");
-const optionD= document.getElementById("optionD");
+let question= document.getElementById("question");
+let optionA= document.getElementById("optionA");
+let optionB= document.getElementById("optionB");
+let optionC= document.getElementById("optionC");
+let optionD= document.getElementById("optionD");
  
-var questions =["Will this show up?" , "What about this one?", "Do you see me?", "Click me?"]
-var options = ["answerA,answerB,answerC,answerD,correctAnswer", "answerA,answerB,answerC,answerD,correctAnswer","answerA,answerB,answerC,answerD,correctAnswer","answerA,answerB,answerC,answerD, correctAnswer"]
+var questions =["Which array method mutates the existing array? " , "What about this one?", "Do you see me?", "Click me?"]
+var options = ["array.map(),array.slice(),array.concat(),array.splice(),array.splice()", "answerA,answerB,answerC,answerD,correctAnswer","answerA,answerB,answerC,answerD,correctAnswer","answerA,answerB,answerC,answerD, correctAnswer"]
 
-let questionOne = questions[0]
-let qOneOptions= options[0]
+// I need a function to iterate over the questions array to make this code input all the questions when needed
 
-console.log(qOneOptions)
-console.log(typeof qOneOptions)
+question.textContent = questions[0];
+
+// let qOneOptions= options[0];
+
+
+// console.log(qOneOptions)
+// console.log(typeof qOneOptions)
+// console.log(questionOne);
+
+function answerOptions (){
+     splitOptions = options.split(",");
+     console.log(splitOptions)
+      
+};
+
+function questionPopulate (){
+    for (let i= 0; i< questions.length; i++){
+    // questionOne = questions[i];
+    // question.textContent = questionOne;
+    question.textContent = questions[i];
+    console.log(questions[i]);
+    console.log(questionOne);
+}
+
+    singleQuestions = questions.indexof[0].split(",");
+    console.log(singleQuestions);
+};
+
+//Which array method mutates the existing array? array.map(), array.slice(), array.concat(), array.splice()
+// Arrays can be used to store
+//What is a function used for?
