c3e7be296f1da41abeecdc2c55b90c2a04966a1b
Janet Iqal <j.iqal35@gmail.com>
2021-06-26 18:20:21 -0500

    added the start function

--- assets/script.js
+++ assets/script.js
@@ -3,14 +3,16 @@ let optionA= document.getElementById("optionA");
 let optionB= document.getElementById("optionB");
 let optionC= document.getElementById("optionC");
 let optionD= document.getElementById("optionD");
+let startGameButton= document.getElementById("button");
+let timer = document.getElementById("timer")
 
-var questoinsAndOptions=[
+var questionsAndOptions=[
 {
 "question" : "Which array method mutates the existing array? " ,
-"option1" :"array.map()",
-"option2" :"array.slice()",
-"option3" : "array.splice()",
-"option4" : "array.concat()",
+"option" :"array.map()",
+"option" :"array.slice()",
+"option" : "array.splice()",
+"option" : "array.concat()",
 "CorrectAnswer": "array.splice()"
 },
 {
@@ -47,50 +49,21 @@ var questoinsAndOptions=[
 }]
 
 
+startGameButton.addEventListener("click", startGame()) ;
 
 function startGame(){
-    document.getElementById("button").innerHTML="hi";
+   console.log("letsgo");
+   //timer needs to start 
+   //call the question/optoins function to populate.
 }
+function questionPopulate() {
+    question.textContent= questionsAndOptions[index].question
+   for
+};
 
-
-
-
-document.getElementById("button").addEventListener("click", startGame()) ;
-function startGame(){
-    document.getElementById("button").innerHTML="hi";
-}
 
 
 // I need a function to iterate over the questions array to make this code input all the questions when needed
-console.log(questoinsAndOptions[0]);
-// question.textContent = questions[0];
-
-// let qOneOptions= options[0];
+// console.log(questionsAndOptions[0]);
 
 
-// console.log(qOneOptions)
-// console.log(typeof qOneOptions)
-// console.log(questionOne);
-
-function answerOptions (){
-     splitOptions = options.split(",");
-     console.log(splitOptions)
-      
-};
-
-function questionPopulate (){
-    for (let i= 0; i< questions.length; i++){
-    // questionOne = questions[i];
-    // question.textContent = questionOne;
-    question.textContent = questions[i];
-    console.log(questions[i]);
-    console.log(questionOne);
-}
-
-    singleQuestions = questions.indexof[0].split(",");
-    console.log(singleQuestions);
-};
-
-//Which array method mutates the existing array? array.map(), array.slice(), array.concat(), array.splice()
-// Arrays can be used to store
-//What is a function used for?
