903daf9ffbf22141c9cad4b9011b83a4b4262c3f
Janet Iqal <j.iqal35@gmail.com>
2021-06-26 20:25:13 -0500

    question populate w function upon start btn clicked

--- assets/script.js
+++ assets/script.js
@@ -5,14 +5,15 @@ let optionC= document.getElementById("optionC");
 let optionD= document.getElementById("optionD");
 let startGameButton= document.getElementById("button");
 let timer = document.getElementById("timer")
+let index =0 ;
 
 var questionsAndOptions=[
-{
+{ 
 "question" : "Which array method mutates the existing array? " ,
-"option" :"array.map()",
-"option" :"array.slice()",
-"option" : "array.splice()",
-"option" : "array.concat()",
+"option1" :"array.map()",
+"option2" :"array.slice()",
+"option3" : "array.splice()",
+"option4" : "array.concat()",
 "CorrectAnswer": "array.splice()"
 },
 {
@@ -24,12 +25,12 @@ var questionsAndOptions=[
 "CorrectAnswer": "All of the above"
 },
 {
-"question" : "Do you see me?",
-"option1" :"answerA",
-"option2" :"answerB",
-"option3" : "answerC",
-"option4" : "aanswerC",
-"CorrectAnswer": "answerC"
+"question" : "How do you properly call a function?",
+"option1" :"function.()",
+"option2" :"function{}",
+"option3" : "function()",
+"option4" : "function({})",
+"CorrectAnswer": "function()"
 },
 {
 "question" : "Click me",
@@ -49,21 +50,33 @@ var questionsAndOptions=[
 }]
 
 
-startGameButton.addEventListener("click", startGame()) ;
+startGameButton.addEventListener("click", startGame) ;
 
 function startGame(){
    console.log("letsgo");
+   questionPopulate();
+   
    //timer needs to start 
    //call the question/optoins function to populate.
 }
 function questionPopulate() {
-    question.textContent= questionsAndOptions[index].question
-   for
+    question.textContent= questionsAndOptions[index].question;
+    optionA.textContent=questionsAndOptions[index].option1;
+    optionB.textContent=questionsAndOptions[index].option2;
+    optionC.textContent=questionsAndOptions[index].option3;
+    optionD.textContent=questionsAndOptions[index].option4;
+
+    optionA.addEventListener("click", answerCheck)
+    optionB.addEventListener("click", answerCheck)
+    optionC.addEventListener("click", answerCheck)
+    optionD.addEventListener("click", answerCheck)
 };
+function answerCheck(event){
+    if 
+};
+// add event listener for each of those options 
+//create a function to check if the answer selected === questionsAndOptions[index].correctAnwer;
+//incremeent the index and go to the next question 
 
-
-
-// I need a function to iterate over the questions array to make this code input all the questions when needed
-// console.log(questionsAndOptions[0]);
-
+// "CorrectAnswer": "array.splice()"
 
